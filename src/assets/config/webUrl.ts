fd5b8499af0bb02765f94fd39887c688c66d4a61
it-desenvolvimento-doureca <it.desenvolvimento@doureca.pt>
2022-01-12 10:22:51 +0000

    fix

--- src/assets/config/webUrl.ts
+++ src/assets/config/webUrl.ts
@@ -14,7 +14,7 @@ export var webUrl = {
   host_app: 'http://192.168.40.126:8080/app',
   host: 'http://192.168.40.126:8080/sgiid',
   link: location.protocol + '//' + location.host.replace('4200', '5050').replace('8080', '5050') + '/dev/sgiid/'
-  /*  */
+  /*  y
   mod_pro: true,
   host_app: location.protocol + '//' + location.host.replace('4200', '8080') + '/app',
   host: location.protocol + '//' + location.host.replace('4200', '8080') + '/sgiid',
